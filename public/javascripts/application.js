8915ff32593f895bd8c7e97cfdc1252293a12307
Fabrício Ferrari de Campos <ffc.fabricio@gmail.com>
2010-09-30 18:11:36 -0300

    #new adicionado footer com os créditos

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,4 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
    // Enable in-place-edit when document is loaded
-$(document).ready(function(){
-
-  $(".in-place-edit").children().inPlaceEdit({
-    submit : submit_handler,
-    cancel : cancel_handler
-  });            
-});
 
