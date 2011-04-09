b93202196499b72a568e6fe9a468e60ac977c33d
Fabrício Ferrari de Campos <ffc.fabricio@gmail.com>
2011-04-09 14:10:53 -0300

    arrumado usuário demo

--- db/migrate/20110409164026_add_encryptable_to_users.rb
+++ db/migrate/20110409164026_add_encryptable_to_users.rb
@@ -1,7 +1,6 @@
 class AddEncryptableToUsers < ActiveRecord::Migration
   def self.up
     change_table(:users) do |t|
-      execute "ALTER TABLE users DROP COLUMN password_salt"
       t.encryptable
     end
   end
