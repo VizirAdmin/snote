58d9ef983e558bd7307c5f6c87de5783aec0fd69
Fabrício Ferrari de Campos <ffc.fabricio@gmail.com>
2011-11-13 00:04:01 -0200

    updated credits info

--- db/schema.rb
+++ db/schema.rb
@@ -50,8 +50,8 @@ ActiveRecord::Schema.define(:version => 20110619143728) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                               :default => "", :null => false
-    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "email",                               :default => "",         :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "",         :null => false
     t.string   "reset_password_token"
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",                       :default => 0
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(:version => 20110619143728) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_salt"
-    t.string   "username"
+    t.string   "username",                            :default => "Anônimo"
     t.string   "language"
   end
 
